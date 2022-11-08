d000cef220f385f1596719e786ddcfb35734d2cf
Valdano Simão <valdanosimao@gmail.com>
2022-11-08 09:57:00 -0300

    estudando operadores de controle de fluxo

--- src/main/kotlin/controle_de_fluxo/operadores_controle_fluxo.kt
+++ src/main/kotlin/controle_de_fluxo/operadores_controle_fluxo.kt
@@ -12,5 +12,33 @@ package controle_de_fluxo
  * Comparação de igualdades
  * Igual ==
  * Diferente !=
- * Negação !
+ * Negação ! //inverte o valor da condição booleana, ex: se for true, com a negação !true ele se torna false
+ */
+
+/*
+ * Tabela verdade
+ *  a    |   b     | resultado
+ * true  |  false  |  false
+ * false |  true   |  false
+ * false |  false  |  false
+ * true  |  true   |  true
+ *  ----------------------------
+ * Operadores lógicos
+ * Usados para avaliar uma relação entre valores
+ * Um operador de conjunção só é avalidado como verdadeiro, caso os dois for verdade
+ *
+ * Conjunção && 'and' //uma condição 'e' outra conjunção
+ * Disjunção || 'or' //só não é executada quando as duas condição for false
+ *
+ * Prioridades:
+ * Parênteses
+ * !Negação
+ * &&Conjução
+ * ||Disjunção
+ */
+
+/*
+ * Operador Range
+ * Usado apra avalier um intervalo de valores
+ * if (n in 1..50){} // mesma coisa que: if(n >= 1 && n <= 50){}
  */
